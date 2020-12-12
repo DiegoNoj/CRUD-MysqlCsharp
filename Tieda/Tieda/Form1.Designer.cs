c14d720e28cb340b9bb610ad18e079fe4ef7d202
Diego Noj <diegonojmatias@gmail.com>
2020-12-12 00:36:34 -0600

    Initial commit

--- Tieda/Tieda/Form1.Designer.cs
+++ Tieda/Tieda/Form1.Designer.cs
@@ -194,6 +194,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
